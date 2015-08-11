8b59ece1a6899fdc055591da72a5218c1d403a2a
brad mccormack <brad_mccormack@exemail.com.au>
2015-08-11 01:00:53 +0000

    - Add install and setup shell files from Magetest stand to adapt

---
+++ docker/MageTestStand/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -x
+
+#Adapted from https://raw.githubusercontent.com/AOEpeople/MageTestStand/master/install.sh
+
+function cleanup {
+	if [ -z $SKIP_CLEANUP ]; then
+		echo "Removing build directory ${BUILDENV}"
+		rm -rf "${BUILDENV}"
+	fi
+}
+
+trap cleanup EXIT
+
+# check if this is a travis environment
+if [ ! -z $TRAVIS_BUILD_DIR ] ; then
+	WORKSPACE=$TRAVIS_BUILD_DIR
+fi
+
+if [ -z $WORKSPACE ] ; then
+	echo "No workspace configured, please set your WORKSPACE environment"
+	exit
+fi
+
+BUILDENV=`mktemp -d /tmp/mageteststand.XXXXXXXX`
+
+echo "Using build directory ${BUILDENV}"
+
+git clone https://github.com/AOEpeople/MageTestStand.git "${BUILDENV}"
+cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/"
+${BUILDENV}/install.sh
+if [ -d "${WORKSPACE}/vendor" ] ; then
+	cp -rf ${WORKSPACE}/vendor/* "${BUILDENV}/vendor/"
+fi
+
+cd ${BUILDENV}/htdocs
+${BUILDENV}/bin/phpunit --colors -d display_errors=1
