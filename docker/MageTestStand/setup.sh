d7682da54e0e96f3132a31aeb459d0f2b6f06362
brad mccormack <brad_mccormack@exemail.com.au>
2015-09-11 03:29:30 +0000

    - Fix

--- docker/MageTestStand/setup.sh
+++ docker/MageTestStand/setup.sh
@@ -38,17 +38,4 @@ fi
 
 
 
-#Populate the initial testing database
-${BUILDENV}/bin/phpunit --colors -d display_errors=1 --group EcomDev_PHPUnitTest
-
-
-if [ -z $TESTS ] ; then
-	echo "Tests were not enabled. Deploying container with Magento + Codisto Connect for manual testing / dev"
-else
-
-	echo "Running CodistoConnect tests .."
-
-	${BUILDENV}/bin/phpunit --colors -d display_errors=1 --group Codisto_Sync
-fi
-
 
