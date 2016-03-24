4397d2174ad7a70ee174e3504c5ea863caa15655
brad mccormack <brad_mccormack@exemail.com.au>
2016-03-24 13:06:58 +1100

    - Add Magento 2 placeholders

--- tasks/plugin/include/startpluginlistener.sh
+++ tasks/plugin/include/startpluginlistener.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#Start FastCGI wrappers over bash scripts
 sandringham -pluginbuildmagento ${HOME}/approot/CodistoConnectQA/tasks/plugin/magento/plugin.sh \"{GITHUBTOKEN} {PLUGINPATH} {BUILDPATH} {TEST} {RESELLERKEY}\" \
+				-pluginbuildmagento2 ${HOME}/approot/CodistoConnectQA/tasks/plugin/magento2/plugin.sh \"{GITHUBTOKEN} {PLUGINPATH} {BUILDPATH} {TEST} {RESELLERKEY}\" \
 				-pluginbuildwoo ${HOME}/approot/CodistoConnectQA/tasks/plugin/woo/plugin.sh \"{GITHUBTOKEN} {PLUGINPATH} {BUILDPATH} {TEST} {RESELLERKEY} {SVNUSER} {SVNPASS}\"\
 				-githelper ${HOME}/approot/CodistoConnectQA/include/githelper.sh \"{PATH} {SHA1} {STASHPOP} {TEST}\"\
 				-listen 127.0.0.1 6971 -children 1 -service
---
+++ tasks/plugin/magento2/plugin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+logger -s "Codisto Connect for Magento 2 plugin build commencing ..."
+
+#Global Vars
+PLUGINNAME="Marketplace Connect"
+DEBUGSTDOUT=0
+DEBUGSYSLOG=1
+DEBUGVERBOSE=1
+
+GITHUBTOKEN=${1}
+PLUGINPATH=${2}
+BUILDPATH=${3}
+TEST=${4}
+RESELLERKEY=${5}
+CONFIGFILE="${PLUGINPATH}/code/community/Codisto/Sync/etc/config.xml"
+GITHUBARTIFACT="CodistoConnect-Magento2.zip"
